f78694df9e203bc30d494ddc4051100ed77eb5f5
arvin <arvinhow95@gmail.com>
2025-04-27 12:03:20 +0800

    add media

--- dazzle-capacitor/capacitor.config.ts
+++ dazzle-capacitor/capacitor.config.ts
@@ -9,14 +9,17 @@ const config: CapacitorConfig = {
   appName: 'Dazzle GPT',
   webDir: 'public',
   server: {
-    // url: "https://dazzlegpt.mullion.io",
-    url: "http://192.168.1.50:5173",
+    url: "https://dazzlegpt.mullion.io",
+    // url: "http://192.168.1.50:5173",
     cleartext: true
   },
   appendUserAgent: "capacitor",
   plugins: {
     StatusBar: {
       overlayWebView: true,
+    },
+    Media: {
+      androidGalleryMode: true
     }
   },
 };
